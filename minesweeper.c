61a96f71dbacf46df2f23e2ffac243f647a93690
RafaCS2002 <rafaelsampaio200802@gmail.com>
2022-10-06 16:34:26 -0300

    print feito

--- minesweeper.c
+++ minesweeper.c
@@ -1,8 +1,51 @@
+#define n 7
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 
+void printCamp(char camp[n][n],int choice[n][n]);
+
 int main(){
-   int camp[7][7];
+   char camp[n][n];
+   static int choice[n][n];
+
    
+
+   // inicializa camp com X em td
+   // for(int i=0;i<n;i++) for(int j=0;j<n;j++) camp[i][j] = 'X';
+   
+   //printa matriz
+   // for(int i=0;i<n;i++) for(int j=0;j<n;j++) if(j==n-1){printf("%c \n",camp[i][j]);}else{printf("%c ",camp[i][j]);}
+   // for(int i=0;i<n;i++) for(int j=0;j<n;j++) if(j==n-1){printf("%d \n",choice[i][j]);}else{printf("%d ",choice[i][j]);}
+
+   printCamp(camp,choice);
+
+}
+
+void printCamp(char camp[n][n],int choice[n][n]){
+   char line[n+1];
+   line[n] = '\n';
+   printf(" ");
+   for(int i=0;i<n;i++){
+      printf("   %d",i);
+   }
+   printf("\n");
+
+   for(int i=0;i<n;i++){
+      //Checar se há escolha
+      for(int j=0;j<n;j++){
+         if(choice[i][j] != 0){
+            line[j] = camp[i][j];
+         }else{
+            line[j] = '-';
+         }
+      }
+
+      //print linha
+      printf("%d |",i);
+      for(int j=0;j<n;j++){
+         printf(" %c |",line[j]);
+      }
+      printf("%c",line[n]);
+   }
 }
