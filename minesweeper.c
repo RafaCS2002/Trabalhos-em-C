41d0151a4d7e6f6d5fe6bd0dfeaa98b32f1240c4
RafaCS2002 <rafaelsampaio200802@gmail.com>
2022-10-12 12:15:10 -0300

    Aleatorizar bombas -- OK

--- minesweeper.c
+++ minesweeper.c
@@ -4,16 +4,18 @@
 #include <stdlib.h>
 
 void printCamp(char camp[n][n],int choice[n][n]);
+void criaCamp(char camp[n][n],int choice[n][n]);
 
 int main(){
-   char camp[n][n];
-   static int choice[n][n];
-   int iChoice, jChoice;
+   char camp[n][n]; // Vai receber o Campo gerado
+   static int choice[n][n]; // 0 - Não escolhido | 1 - Escolhido
+   int iChoice, jChoice; // Escolha linha e coluna
    int loop = 1;
+   int first = 1;
 
 
-   // inicializa camp com X em td
-   for(int i=0;i<n;i++) for(int j=0;j<n;j++) camp[i][j] = 'X';
+   // inicializa camp com '' em td
+   for(int i=0;i<n;i++) for(int j=0;j<n;j++) camp[i][j] = '.';
 
    while (loop){
       
@@ -25,8 +27,15 @@ int main(){
       scanf("%d",&jChoice);
 
       choice[iChoice][jChoice] = 1;
+
+      // Checa se é a primeira vez que está sendo escolhido
+      if (first){
+         criaCamp(camp,choice);
+         first=0;
+      }
+
       //printa matriz
-      // for(int i=0;i<n;i++) for(int j=0;j<n;j++) if(j==n-1){printf("%c \n",camp[i][j]);}else{printf("%c ",camp[i][j]);}
+      for(int i=0;i<n;i++) for(int j=0;j<n;j++) if(j==n-1){printf("%c \n",camp[i][j]);}else{printf("%c ",camp[i][j]);}
       // for(int i=0;i<n;i++) for(int j=0;j<n;j++) if(j==n-1){printf("%d \n",choice[i][j]);}else{printf("%d ",choice[i][j]);}
    }
 }
@@ -34,6 +43,7 @@ int main(){
 void printCamp(char camp[n][n],int choice[n][n]){
    char line[n+1];
    line[n] = '\n';
+   
    printf(" ");
    for(int i=0;i<n;i++){
       printf("   %d",i);
@@ -43,7 +53,7 @@ void printCamp(char camp[n][n],int choice[n][n]){
    for(int i=0;i<n;i++){
       //Checar se há escolha
       for(int j=0;j<n;j++){
-         if(choice[i][j] != 0){
+         if(choice[i][j]){
             line[j] = camp[i][j];
          }else{
             line[j] = '-';
@@ -56,5 +66,55 @@ void printCamp(char camp[n][n],int choice[n][n]){
          printf(" %c |",line[j]);
       }
       printf("%c",line[n]);
+   }
+}
+
+void criaCamp(char camp[n][n],int choice[n][n]){
+   int bomb[2];
+   int choiceIndex[2];
+   int bombInZone,bombInBomb;
+
+   // Recebe os indices da escolha
+   for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+         if (choice[i][j]){
+            choiceIndex[0] = i;
+            choiceIndex[1] = j;
+         }
+      }
+   }
+
+   // Num de bombas == tamanho n da matriz
+   for(int i=0;i<n;i++){ 
+      bomb[0] = (int) floorf(rand()%n); // linha
+      bomb[1] = (int) floorf(rand()%n); // coluna
+      bombInZone = 0;
+      bombInBomb = 0;
+
+      // checa se aleatorizou fora da zona de escolha
+      for(int j=-1;j<=1;j++){
+         for(int k=-1;k<=1;k++){
+            if ((choiceIndex[0]-j == bomb[0]) && (choiceIndex[1]-k == bomb[1])){// ([linha e coluna de escolha]+[-1,0,1]) == [linha e coluna da bomba aleatória]
+               bombInZone=1;
+            }
+         }
+      }
+
+      // Checa se há bomba no campo da linha e coluna aleatorizada
+      for(int j=0;j<n;j++){
+         for(int k=0;k<n;k++){
+            if (camp[j][k]=='X' && (j==bomb[0] && k==bomb[1])){
+               bombInBomb=1;
+            }
+         }
+      }
+
+      if(bombInZone){ // Tem bomba em zona de escolha
+         i--;    // Volta uma iteração
+      }else if(bombInBomb){ // Tem bomba já?
+         i--;    // Volta uma iteração
+      }else{
+         camp[bomb[0]][bomb[1]]='X';
+      }
    }
 }
